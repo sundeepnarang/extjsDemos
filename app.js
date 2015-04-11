eb98796fdfccfc0f5b8ae9a03256eb6dc5daf382
Sundeep Narang <sundeepnrng@gmail.com>
2015-04-11 15:12:14 -0400

    my panel

--- app.js
+++ app.js
@@ -2,19 +2,28 @@
  * Created by Sundeep on 4/11/2015.
  */
 
+Ext.define('MyApp.MyPanel', {
+    extend      : 'Ext.Panel',
+    width       : 200,
+    height      : 150,
+    bodyPadding : 5
+});
+
 Ext.application({
     name   : 'MyApp',
 
     launch : function() {
 
-        Ext.create('Ext.Panel', {
-            renderTo     : Ext.getBody(),
-            width        : 200,
-            height       : 150,
-            bodyPadding  : 5,
-            title        : 'Hello World',
-            html         : 'Hello <b>World</b>...'
+        Ext.create('MyApp.MyPanel', {
+            renderTo :Ext.getBody(),
+            title    : 'My First Panel',
+            html     : 'My First Panel'
         });
 
+        Ext.create('MyApp.MyPanel', {
+            renderTo : Ext.getBody(),
+            title    : 'My Second Panel',
+            html     : 'My Second Panel'
+        });
     }
 });
