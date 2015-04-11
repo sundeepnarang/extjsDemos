7ada2764011d739fc238998f05273255b31766a7
Sundeep Narang <sundeepnrng@gmail.com>
2015-04-11 15:39:59 -0400

    sencha cmd

--- app.js
+++ app.js
@@ -1,38 +1,18 @@
-/**
- * Created by Sundeep on 4/11/2015.
+/*
+ * This file is generated and updated by Sencha Cmd. You can edit this file as
+ * needed for your application, but these edits will have to be merged by
+ * Sencha Cmd when upgrading.
  */
-
 Ext.application({
-    name   : 'MyApp',
+    name: 'MyApp',
 
-    launch : function() {
-
-        Ext.widget({
-            renderTo : Ext.getBody(),
-            xtype    : 'grid',
-            title    : 'Grid',
-            width    : 650,
-            height   : 300,
-            plugins  : 'rowediting',
-            store    : {
-                fields : [ 'name', 'age', 'votes', 'credits' ],
-                data   : [
-                    [ 'Bill', 35, 10, 427 ],
-                    [ 'Fred', 22, 4, 42 ]
-                ]
-            },
-            columns: {
-                defaults: {
-                    editor : 'numberfield',
-                    width  : 120
-                },
-                items: [
-                    { text: 'Name', dataIndex: 'name', flex: 1, editor: 'textfield' },
-                    { text: 'Age', dataIndex: 'age' },
-                    { text: 'Votes', dataIndex: 'votes' },
-                    { text: 'Credits', dataIndex: 'credits' }
-                ]
-            }
-        })
-    }
+    extend: 'MyApp.Application',
+    
+    autoCreateViewport: 'MyApp.view.main.Main'
+	
+    //-------------------------------------------------------------------------
+    // Most customizations should be made to MyApp.Application. If you need to
+    // customize this file, doing so below this section reduces the likelihood
+    // of merge conflicts when upgrading to new versions of Sencha Cmd.
+    //-------------------------------------------------------------------------
 });
